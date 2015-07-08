30be0a97e414d44420c1e7aa9117e17d9fc167ad
Bo Shi <cnshibo@gmail.com>
2015-07-09 00:37:55 +0800

    add build config for Hi3518E platform

---
+++ hi3518e_build.sh
@@ -0,0 +1,6 @@
+./configure --config=ARM-Linux-Hi3518e --omit=CppParser,PDF,MongoDB,Crypto,NetSSL_OpenSSL,Data,PageCompiler  --no-tests  --no-samples --no-sharedmemory --no-wstring  --no-fpenvironment
+
+make
+
+
+
